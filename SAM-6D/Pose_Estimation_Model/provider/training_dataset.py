a54f84f2f18f46a572ceda147b9b13cde6d3931a
SeifAI21 <seifamga447@gmail.com>
2025-04-17 12:45:01 +0200

    added print statement to debbug

--- SAM-6D/Pose_Estimation_Model/provider/training_dataset.py
+++ SAM-6D/Pose_Estimation_Model/provider/training_dataset.py
@@ -136,6 +136,8 @@ class Dataset():
         valid_idx = []
         print(gt_info)
         for k, item in enumerate(gt_info):
+            print(f"Item {k}: {item}")
+            
             if item['px_count_valid'] >= self.min_visib_px and item['visib_fract'] >= self.min_visib_frac:
                 valid_idx.append(k)
         
